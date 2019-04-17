946809c748e562c31747f1ceb91ca4f1ad032624
jfqd <jfqd@blun.org>
2019-04-17 18:10:54 +0200

    remove datasets from datasets.at

--- copy/var/zoneinit/includes/31-dsapid.sh
+++ copy/var/zoneinit/includes/31-dsapid.sh
@@ -41,14 +41,6 @@ if [[ ! -e /data/config.json ]]; then
       "provider": "joyent",
       "source": "https://images.joyent.com/images",
       "delay": "24h"
-    },
-    {
-      "name": "datasets.at",
-      "active": false,
-      "type": "dsapi",
-      "provider": "community",
-      "source": "http://datasets.at/api/datasets",
-      "delay": "24h"
     }
   ]
 }
